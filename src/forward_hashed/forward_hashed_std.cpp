75300066f95550f7fdc438fcdb20ed44ba8a488a
liuzikai <zikai.liu@outlook.com>
2022-06-05 01:09:04 +0200

    Forward hashed std

---
+++ src/forward_hashed/forward_hashed_std.cpp
@@ -0,0 +1,12 @@
+#include "forward_hashed/forward_hashed_std.hpp"
+
+namespace fhstd {
+    template struct ForwardHashedHelper<index_t>;
+
+    template ForwardHashedHelper<index_t> *forward_hashed_create_neighbor_container(const AdjacencyGraph<index_t> *G);
+
+    template void forward_hashed_delete_neighbor_container<index_t>(ForwardHashedHelper<index_t> *A);
+
+    template void forward_hashed(TriangleListing::Count<index_t>* tlr, AdjacencyGraph<index_t> *G, ForwardHashedHelper<index_t> * A);
+    template void forward_hashed(TriangleListing::Collect<index_t>* tlr, AdjacencyGraph<index_t> *G, ForwardHashedHelper<index_t> * A);
+}
